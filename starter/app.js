ec56778870455f0f47e50147972cef7463c20bd7
Guilherme Bernardo <bguilherme51@gmail.com>
2022-03-17 14:41:19 -0400

    task manager initial definitions and configurations!

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express');
+const app  = express();
+
+
+//routes
+app.get("/hello", (req, res) => {
+	res.send("task manager starting work!!")
+})
+
+
+const port = 3000
+app.listen(port, console.log(`server's listening on port ${port}...`))
